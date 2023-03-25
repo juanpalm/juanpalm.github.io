dff03c3775627b90d031621e5935ef7815b36afe
juanpalm <127349850+juanpalm@users.noreply.github.com>
2023-03-24 21:03:33 -0300

    actualizacion
    estuve trabajando sobre el archivo, todavía no pude hacer la interacción deseada

---
+++ function.js
@@ -0,0 +1,19 @@
+
+console.log("Thomas Molina CV")
+
+function Experiencia() {
+    onclick=document.getElementById("boton").innerHTML=("La experiencia esta por venir");
+}
+
+/** 
+<button onclick=document.getElementById('boton').innerHTML=("La experiencia esta por venir")")>
+*/
+
+/** 
+document.getElementById('formulario').addEventListener('click', function (click){
+
+console.log("Gracias por contactarse conmigo");
+
+
+
+})*/
